5b98ab0ca13206211fc54607993b99c8d1550b4b
cukis17 <matias.vergara5445@gmail.com>
2025-06-15 21:19:35 -0400

    actualizacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-
+import react from "react";
+import reactDOM from "react-dom/client";
+import AppRouter from "./router";
 import "bootstrap/dist/css/botstrap.min.css";
-import "fortawesome/fontawesome-free/css-all.min.css";
+import "@fortawesome/fontawesome-free/css-all.min.css";
 
-import "./index.css"
-import App from ".App.jsx"
-
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<AppRouter/>);
