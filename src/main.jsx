3ad72e3bf48704b17c70155006290d1f731c52a9
cukis17 <matias.vergara5445@gmail.com>
2025-06-19 19:42:27 -0400

    archivos guia 1 firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./router";
+import {BrowserRouter} from "react-router-dom";
+import {AuthProvider} from "./context/AuthProvider";
 import "bootstrap/dist/css/botstrap.min.css";
 import "@fortawesome/fontawesome-free/css-all.min.css";
 
