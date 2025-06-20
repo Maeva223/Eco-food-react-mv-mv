ce26ee29063a813ea41c62d07a1277a490dd6f8a
cukis17 <matias.vergara5445@gmail.com>
2025-06-19 20:30:47 -0400

    actualizacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AppRouter from "./router";
-import {BrowserRouter} from "react-router-dom";
-import {AuthProvider} from "./context/AuthProvider";
-import "bootstrap/dist/css/botstrap.min.css";
-import "@fortawesome/fontawesome-free/css-all.min.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(<AppRouter/>);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,14 @@
 import CardProducto from '../components/CardProducto';
+import CerrarSesion from "../components/CerrarSesion";
+
+function Home() {
+    return (
+    <div>
+        <h2>Bienvenido a EcoFood</h2>
+        <CerrarSesion />
+    </div>
+    );
+}
 
 function Home(){
     return (
