59685947a14d879eb5668ed21bcb6f2387d7a633
cukis17 <matias.vergara5445@gmail.com>
2025-06-21 21:05:38 -0400

    actualizacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
-import { StrictMode } from 'react'
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import AppRouter from "./routes/AppRouter.jsx";
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from "./context/AuthProvider.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
