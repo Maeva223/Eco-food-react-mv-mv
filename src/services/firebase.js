be25be47758730c3818c934b5a681bb24a860e13
cukis17 <matias.vergara5445@gmail.com>
2025-06-22 17:17:25 -0400

    actualizacion

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrFwEuKdZk1walAI5AIEvwNYfazIz54gU",
-    authDomain: "ecofood-app-8e771.firebaseapp.com.firebaseapp.com",
-    projectId: "ecofood-app-8e771",
-    storageBucket: "ecofood-app-8e771.firebasestorage.app",
-    messagingSenderId: "1006055374200",
-    appId: "1:1006055374200:web:10c7236b27d207e2569c8d"
+  apiKey: "AIzaSyCrFwEuKdZk1walAI5AIEvwNYfazIz54gU",
+  authDomain: "ecofood-app-8e771.firebaseapp.com",
+  projectId: "ecofood-app-8e771",
+  storageBucket: "ecofood-app-8e771.appspot.com",
+  messagingSenderId: "1006055374200",
+  appId: "1:1006055374200:web:10c7236b27d207e2569c8d"
 };
 
 const app = initializeApp(firebaseConfig);
